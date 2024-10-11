6db70a566445b7e3344076da5d1057dbf46116e9
adihere <aditya.vadaganadam@gmail.com>
2024-10-11 19:31:51 +0100

    added new URLs

--- src/app/components/Context/urls.ts
+++ src/app/components/Context/urls.ts
@@ -1,22 +1,101 @@
-export const urls = [{
-  url: "https://www.opengroup.org/architecture/togaf8/",
-  title: "TOGAF Architecture Methodology",
-  seeded: false,
-  loading: false,
-},
-{
-  url: "https://github.com/joelparkerhenderson/architecture-decision-record",
-  title: "Architecture Decision Records - Guidelines",
-  seeded: false,
-  loading: false,
-},
-{
-  url: "https://ieeexplore.ieee.org/document/875998/figures#figures",
-  title: " IEEE Recommended Practice for Architectural Description for Software-Intensive Systems",
-  seeded: false,
-  loading: false,
-}]
-
+export const urls = [
+  {
+    url: "https://adr.github.io/",
+    title: "Architecture Decision Records",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://www.ozimmer.ch/practices/2023/04/03/ADRCreation.html",
+    title: "Architecture Decision Records - Creation",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://ieeexplore.ieee.org/document/9801811",
+    title: "IEEE Xplore Document",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://github.com/joelparkerhenderson/architecture-decision-record/blob/main/locales/en/templates/decision-record-template-by-michael-nygard/index.md",
+    title: "Decision Record Template by Michael Nygard",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://www.thoughtworks.com/en-us/radar/techniques/lightweight-architecture-decision-records",
+    title: "Lightweight Architecture Decision Records - ThoughtWorks",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://www.infoq.com/articles/sustainable-architectural-design-decisions/",
+    title: "Sustainable Architectural Design Decisions - InfoQ",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://schubmat.github.io/DecisionCapture/",
+    title: "Decision Capture",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://schubmat.github.io/DecisionCapture/templates/captureTemplate_simple.html",
+    title: "Simple Decision Capture Template",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://www.ozimmer.ch/practices/2022/11/22/MADRTemplatePrimer.html",
+    title: "MADR Template Primer",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://www.ozimmer.ch/practices/2020/05/22/ADDefinitionOfDone.html",
+    title: "Architecture Decision Definition of Done",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://www.ozimmer.ch/practices/2023/04/05/ADRReview.html",
+    title: "Architecture Decision Record Review",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://www.ozimmer.ch/practices/2023/04/21/ADAdoptionModel.html",
+    title: "Architecture Decision Adoption Model",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://openpracticelibrary.com/practice/architectural-decision-records-adr/",
+    title: "Architectural Decision Records (ADR) - Open Practice Library",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://www.ost.ch/de/forschung-und-dienstleistungen/informatik/ifs-institut-fuer-software/labs/cloud-application-lab/architectural-knowledge-management-akm",
+    title: "Architectural Knowledge Management (AKM)",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://socadk.github.io/design-practice-repository/",
+    title: "Software Architecture Design Practice Repository",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://cognitect.com/blog/2011/11/15/documenting-architecture-decisions.html/",
+    title: "Documenting Architecture Decisions - Cognitect Blog",
+    seeded: false,
+    loading: false,
+  }
+];
 
 /*
  future additions
