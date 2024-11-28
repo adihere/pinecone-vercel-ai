b2a0d69818b632d4d70bdec7feb5e24e7216d401
adihere <aditya.vadaganadam@gmail.com>
2024-11-28 17:48:26 +0000

    fixed path of debug.log inot /tmp directory

--- src/app/api/log-debug/route.ts
+++ src/app/api/log-debug/route.ts
@@ -4,9 +4,10 @@ import path from 'path';
 
 export async function POST(request: Request) {
   try {
+    const LOGS_FILE_PATH = '/tmp/debug.log';
     const { info } = await request.json();
-    const logPath = path.join(process.cwd(), 'src', 'app', 'validate', 'debug.log');
-    await fs.appendFile(logPath, `${new Date().toISOString()}: ${info}\n`);
+    //const logPath = path.join(process.cwd(), 'src', 'app', 'validate', 'debug.log');
+    await fs.appendFile(LOGS_FILE_PATH, `${new Date().toISOString()}: ${info}\n`);
     return NextResponse.json({ message: 'Debug info logged successfully' }, { status: 200 });
   } catch (error) {
     console.error('Error writing to debug log:', error);
